9781c4aa7f66d399a71b5bfd62da44ec1e797256
HP <walfgangkelly@gmail.com>
2023-03-25 18:34:07 -0700

    fixing Bugs and refactoring version1

--- main.py
+++ main.py
@@ -39,9 +39,9 @@ def chat():
             img_list.append(img_src)
             # print(img_src)
 
-    return render_template("index.html", first_header= "wolrd", pic1=img_list[0],
-                         pic2=img_list[1], pic3=img_list[2], pic4=img_list[3], pic5=img_list[4],
-                           title1=title_list[0], title2=title_list[1], title3=title_list[2], title4=title_list[3], title5=title_list[4])
+    return render_template("index.html", pic1=img_list[0],pic2=img_list[1], pic3=img_list[2],
+                           pic4=img_list[3], pic5=img_list[4], title1=title_list[0], title2=title_list[1],
+                           title3=title_list[2], title4=title_list[3], title5=title_list[4])
 
 
 
